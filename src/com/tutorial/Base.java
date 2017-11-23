b4ce05ddaa5fa4b9568e0550c6cb0f4b05cd99a9
Ihor Chyzh <ichyzh@gmail.com>
2017-11-23 22:13:41 +0200

    Add first test base

---
+++ src/com/tutorial/Base.java
@@ -0,0 +1,27 @@
+package com.tutorial;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Base {
+
+    public static AndroidDriver<AndroidElement> Capabilities() throws MalformedURLException {
+
+        File appDir = new File("src");
+        File app = new File(appDir,"ApiDemos-debug.apk");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"07a210a70c858886");
+        capabilities.setCapability(MobileCapabilityType.APP,app.getAbsolutePath());
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        return driver;
+
+        // Android UI Automator
+
+    }
+}
